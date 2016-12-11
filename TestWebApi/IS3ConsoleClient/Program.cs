c3d5b91cc0f064eec7b6aad1da371c1f2f8a8602
andrey192 <crusher_192@mail.ru>
2016-12-11 08:02:47 +0300

    TestWebApi

---
+++ TestWebApi/IS3ConsoleClient/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using IdentityModel.Client;
+using System.Net.Http;
+
+namespace IS3ConsoleClient
+{
+    class Program
+    {
+
+
+    static TokenResponse GetClientToken()
+        {
+            var client = new TokenClient(
+                "http://localhost:5000/connect/token",
+                "silicon",
+                "F621F470-9731-4A25-80EF-67A6F7C5F4B8");
+
+            return client.RequestClientCredentialsAsync("api1").Result;
+        }
+
+        static void CallApi(TokenResponse response)
+        {
+            var client = new HttpClient();
+            client.SetBearerToken(response.AccessToken);
+
+            Console.WriteLine(client.GetStringAsync("http://localhost:14869/test").Result);
+        }
+        static void Main(string[] args)
+        {
+        }
+    }
+}
